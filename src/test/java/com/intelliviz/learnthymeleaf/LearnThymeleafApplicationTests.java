3bd619990559953b35ef433d36a97f6046bac2c9
emuhlestein <emuhlestein@yahoo.com>
2020-09-26 06:54:40 -0500

    Initial project commit

---
+++ src/test/java/com/intelliviz/learnthymeleaf/LearnThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intelliviz.learnthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
