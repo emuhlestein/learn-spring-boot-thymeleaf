5a802c131dde657a115c120ee13b794ad482e783
emuhlestein <emuhlestein@yahoo.com>
2020-09-28 15:12:55 -0500

    Style login page, add welcome page.

--- src/main/java/com/intelliviz/learnthymeleaf/controllers/LoginController.java
+++ src/main/java/com/intelliviz/learnthymeleaf/controllers/LoginController.java
@@ -2,12 +2,21 @@ package com.intelliviz.learnthymeleaf.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
     @GetMapping("/login")
-    public String login() {
+    public String showLoginView() {
         return "login";
+    }
+
+    @PostMapping("/login")
+    public String handleLoginRequest(@RequestParam String name, @RequestParam String password) {
+        System.out.println("Name: " + name);
+        System.out.println("Password: " + password);
+        return "welcome";
     }
 }
